6ffc745ddfb0e5102b295bc0b6e75df3c824d881
Alessandro Tiberia <tiberia7030@gmail.com>
2026-03-20 16:52:23 +0100

    consegna esercizio 2 completo

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,17 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const Name = "Alessandro";
+console.log(Name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 10;
+const text = "Monkey";
+const bool= true; // cosi do un valore booleano
+console.log(number, text, bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +20,13 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + " " + lastName; //dico che full name è la somma tra nome + cognome ("" è per dare spazio)
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+
+//devo contare quanti caratteri ci sono dentro la variabile alphabet
+//per farlo si usa lenght quindi richiamo la variabile e aggiungo lenght per contare.
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
